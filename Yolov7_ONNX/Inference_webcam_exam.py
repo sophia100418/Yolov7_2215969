7b3167c41b982a96a96f76446934a57114782766
sojeong2215969 <165887301+sophia100418@users.noreply.github.com>
2024-04-24 17:58:11 +0900

    MidtermExam Code Fix. Opensource 2024/04/24

--- Yolov7_ONNX/Inference_webcam_exam.py
+++ Yolov7_ONNX/Inference_webcam_exam.py
@@ -18,11 +18,10 @@ from collections import OrderedDict,namedtuple
 providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] if cuda else ['CPUExecutionProvider'] #['AzureExecutionProvider', 'CPUExecutionProvider'] if cuda else ['CPUExecutionProvider']
 session = ort.InferenceSession(w, providers=providers)
 
-tf.compat.disable_v2_behavior()
-with tf.compat.Session() as sess:
-    x = tf.compat.placeholder(tf.float32, [2])
-    x2 = tf.square(x)
-    print(sess.run(x2, feed_dict={x: [2, 3]}))
+x = tf.constant([2, 3], dtype=tf.float32)
+x2 = tf.square(x)
+print(x2.numpy())
+
     # [4. 9.]
 
 #print("00000")
